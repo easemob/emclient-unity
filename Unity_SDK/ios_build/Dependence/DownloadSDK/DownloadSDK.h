7dd1517a33a4a35e25be8e55bc1c9fc1e835b72e
dujiepeng <347302029@qq.com>
2022-08-05 14:55:44 +0800

    add ios build scripts.

---
+++ Unity_SDK/ios_build/Dependence/DownloadSDK/DownloadSDK.h
@@ -0,0 +1,12 @@
+//
+//  DownloadSDK.h
+//  DownloadSDK
+//
+//  Created by 杜洁鹏 on 2022/7/18.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownloadSDK : NSObject
+
+@end
---
+++ Unity_SDK/ios_build/Dependence/DownloadSDK/DownloadSDK.m
@@ -0,0 +1,12 @@
+//
+//  Download.m
+//  DownloadSDK
+//
+//  Created by 杜洁鹏 on 2022/7/18.
+//
+
+#import "DownloadSDK.h"
+
+@implementation DownloadSDK
+
+@end
