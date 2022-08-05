69a57d8bc895a3aca898de562ac2483e29e83ec4
luckyyuqiang <luckyyuqiang@163.com>
2022-08-05 15:25:16 +0800

    Add windows sdk distribute scripts.

---
+++ Win_SDK/Distribute/Readme.md
@@ -0,0 +1,9 @@
+
+agora_chat_sdk.1.0.2-beta.nupkg:
+--  created based on Unity stable 3.9.0
+--  support .Net FrameWork 4.5.2, 4.6.2, 4.7.2 and 4.8. (Note: All chatsdk.dll are compiled in .Net Framework 4.5.2)
+
+agora_chat_sdk.1.0.5.nupkg:
+--  created based on Unity stable 3.9.3
+--  support .Net FrameWork 4.5.2, 4.6.2, 4.7.2 and 4.8. (Note: All chatsdk.dll are compiled in .Net Framework 4.5.2)
+--  support new features: moderation, reaction, threading and translation.
---
+++ Win_SDK/Distribute/build.bat
@@ -0,0 +1,10 @@
+xcopy /Y ..\Dependency\*.dll runtimes\x64\native\
+
+xcopy /Y ..\..\..\emclient-unity\Unity_SDK\win_build\build_shared\Release\hyphenateCWrapper.dll runtimes\x64\native\
+
+xcopy /Y ..\..\Win_SDK\FrameworkLib\bin\Release\chatsdk.dll lib\net48\
+xcopy /Y ..\..\Win_SDK\FrameworkLib\bin\Release\chatsdk.dll lib\net452\
+xcopy /Y ..\..\Win_SDK\FrameworkLib\bin\Release\chatsdk.dll lib\net462\
+xcopy /Y ..\..\Win_SDK\FrameworkLib\bin\Release\chatsdk.dll lib\net472\
+
+Nuget.exe pack agora_chat_sdk.nuspec
