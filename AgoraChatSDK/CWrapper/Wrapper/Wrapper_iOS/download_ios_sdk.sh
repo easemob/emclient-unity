2f78bdb93163a33efc816436d7dc3e349a0b769b
dujiepeng <347302029@qq.com>
2023-01-05 13:53:48 +0800

    add ios build script

---
+++ AgoraChatSDK/CWrapper/Wrapper/Wrapper_iOS/download_ios_sdk.sh
@@ -0,0 +1,29 @@
+SDKVERISON=3_9_9
+FILE=HyphenateChat.framework
+LOCK=.emlock
+
+if [ ! -f ${LOCK} ]; then
+  touch ${LOCK}
+fi
+
+echo "Target: ${SDKVERISON}"
+
+for LINE in `cat ${LOCK}`
+do
+  echo "Current: ${LINE}"
+done
+
+if [ ! "$LINE" = "$SDKVERISON" ]; then
+  rm -rf ${FILE}
+fi
+
+if [ ! -d ${FILE} ]; then
+  echo "$FILE not exist!! download it..."
+  curl -o HyphenateChat.zip https://downloadsdk.easemob.com/downloads/HyphenateChat${SDKVERISON}.zip
+  unzip HyphenateChat.zip
+  rm -rf HyphenateChat.zip
+  mv HyphenateChat.xcframework/ios-arm64_armv7/HyphenateChat.framework ./HyphenateChat.framework
+  rm -rf HyphenateChat.xcframework
+fi
+
+echo ${SDKVERISON} > ${LOCK}
