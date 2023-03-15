3b06b4c00d2d986713fa1dc0f5fca44a7f4f1ae9
luckyyuqiang <luckyyuqiang@163.com>
2023-03-15 19:43:58 +0800

    fix bug of crash when cannot find callbackid in dictionary.

--- Win_SDK/FrameworkTest/Testor.cs
+++ Win_SDK/FrameworkTest/Testor.cs
@@ -4859,7 +4859,7 @@ namespace WinSDKTest
                     Console.WriteLine($"LoadMessages found {list.Count} messages");
                     foreach (var it in list)
                     {
-                        Console.WriteLine($"message id: {it.MsgId}");
+                        Console.WriteLine($"message id: {it.MsgId}, {it.ServerTime}");
                     }
                 },
                 onError: (code, desc) => {
