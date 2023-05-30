8e9f6900eac9c29e93cb8f93f7ac0642b17fadac
luckyyuqiang <luckyyuqiang@163.com>
2023-05-30 10:50:29 +0800

    Remove useless ChatSDKDemo and Unity_SDK from repo.

--- Unity_SDK/win_build/gen_winsln_shared.bat
+++
@@ -1,5 +0,0 @@
-xcopy hyphenate_shared_win.gyp ..\..\..\
-cd ..\..\..\
-call emclient-linux\tools\gyp-master\gyp --depth=. hyphenate_shared_win.gyp --generator-output=emclient-unity\Unity_SDK\win_build\build_shared -D ENABLE_CALL=0
-del hyphenate_shared_win.gyp
-cd emclient-unity\Unity_SDK\win_build\
