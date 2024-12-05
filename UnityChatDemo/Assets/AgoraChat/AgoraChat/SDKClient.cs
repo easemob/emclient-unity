ad520bab14d8bd4c9851e9f19041c57eef991bc2
luckyyuqiang <luckyyuqiang@163.com>
2024-12-05 19:38:42 +0800

    update Sdkversion to 1.4.0 part 2

--- UnityChatDemo/Assets/AgoraChat/AgoraChat/SDKClient.cs
+++ UnityChatDemo/Assets/AgoraChat/AgoraChat/SDKClient.cs
@@ -131,7 +131,7 @@ namespace AgoraChat
          * \~english
          * The SDK version.
          */
-        public string SdkVersion { get => "1.3.1"; }
+        public string SdkVersion { get => "1.4.0"; }
 
 
         /**
